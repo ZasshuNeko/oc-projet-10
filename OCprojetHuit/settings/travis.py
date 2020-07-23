cdba8aa8f6052b531ac4a71f37d1ec233fb10a0e
Ubuntu <ubuntu@ip-172-31-46-198.eu-west-3.compute.internal>
2020-07-23 09:15:23 +0000

    update travis config

--- OCprojetHuit/settings/travis.py
+++ OCprojetHuit/settings/travis.py
@@ -1,5 +1,13 @@
 from . import *
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'x693c28g7ptqmwg)enro2wuxe*6-_wovm)d&i90k9-gx#&_4=l'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['35.180.208.255','127.0.0.1']
+
+
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
